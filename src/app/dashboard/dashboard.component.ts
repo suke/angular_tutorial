f6d1895e1c19ca830ad72418c5cf0284c0756153
suke <suke083.tech@gmail.com>
2018-09-15 22:38:04 +0900

    finish routing

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,13 +1,14 @@
 import { Component, OnInit } from '@angular/core'
 import { Hero } from '../hero'
 import { HeroService } from '../service/hero/hero.service'
+
 @Component({
-  selector: 'app-heroes',
-  templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css']
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css']
 })
-export class HeroesComponent implements OnInit {
-  heroes: Hero[]
+export class DashboardComponent implements OnInit {
+  heroes: Hero[] = []
 
   constructor(private heroService: HeroService) {}
 
